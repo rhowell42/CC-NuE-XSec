e93234eeed081d495281b624d681326b36fe779d
Ryan Howell <rhowell@minervagpvm04.fnal.gov>
2025-02-25 13:26:42 -0600

    updated submissions scripts

--- selection/FHCselectall.sh
+++ selection/FHCselectall.sh
@@ -1,13 +1,11 @@
 declare -a ncdiffplaylists=("A" "B" "C" "D" "E" "F" "G" "L" "M" "N" "O" "P")
 declare -a playlists=("A" "B" "C" "D" "E" "F" "G" "L" "M" "N" "O" "P")
-#declare -a playlists=("B" "C" "D" "E" "F" "G")
-#declare -a playlists=("L" "M" "N" "O" "P")
 
 echo "RUNNING NCDIFFSIGNAL JOBS"
 
 for i in "${ncdiffplaylists[@]}"
 do
-   python gridSelection.py --playlist me1"$i"_p4_NCDiff --ntuple_tag MAD --use-sideband  dEdX --truth --cal_POT --mc_only --selection_tag thesis --tarball /pnfs/minerva/resilient/tarballs/rhowell-CCNUE_selection_100Universes.tar.gz 
+   python gridSelection.py --playlist me1"$i"_p4_NCDiff --ntuple_tag MAD --use-sideband  dEdX --truth --cal_POT --mc_only --selection_tag paper --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20250218.tar.gz 
 done
 
 echo "DONE RUNNING NCDIFFSIGNAL JOBS"
@@ -15,9 +13,9 @@ echo "RUNNING AUDIT JOBS"
 
 for i in "${playlists[@]}"
 do
-   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband dEdX --cal_POT --mc_only --truth --selection_tag thesis --tarball /pnfs/minerva/resilient/tarballs/rhowell-CCNUE_selection_100Universes.tar.gz 
-   python gridSelection.py --playlist me1"$i"_p4_BigGenie --ntuple_tag MAD --use-sideband dEdX --cal_POT --mc_only --truth --selection_tag thesis --tarball /pnfs/minerva/resilient/tarballs/rhowell-CCNUE_selection_100Universes.tar.gz 
-   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband dEdX --cal_POT --data_only --truth --selection_tag thesis --tarball /pnfs/minerva/resilient/tarballs/rhowell-CCNUE_selection_100Universes.tar.gz 
+   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband dEdX --cal_POT --mc_only --truth --selection_tag paper --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20250218.tar.gz 
+   python gridSelection.py --playlist me1"$i"_p4_BigGenie --ntuple_tag MAD --use-sideband dEdX --cal_POT --mc_only --truth --selection_tag paper --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20250218.tar.gz 
+   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband dEdX --cal_POT --data_only --truth --selection_tag paper --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20250218.tar.gz 
 done
 
 echo "DONE RUNNING AUDIT JOBS"
