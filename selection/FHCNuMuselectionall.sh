bbf2c52e89306cebe8784514c34a09eb8dd8f0f6
Ryan Howell <rhowell@minervagpvm03.fnal.gov>
2025-11-11 15:38:44 -0600

    bash changes

--- selection/FHCNuMuselectionall.sh
+++ selection/FHCNuMuselectionall.sh
@@ -4,8 +4,8 @@ echo "RUNNING AUDIT JOBS"
 
 for i in "${playlists[@]}"
 do
-   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband  --cal_POT --mc_only --selection_tag paper_muon --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20251005.tar.gz
-   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband  --cal_POT --data_only --selection_tag paper_muon --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20251005.tar.gz
+   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband  --cal_POT --mc_only --selection_tag paper_muon --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20251012.tar.gz --scratch
+   python gridSelection.py --playlist me1"$i"_p4 --ntuple_tag MAD --use-sideband  --cal_POT --data_only --selection_tag paper_muon --tarball /pnfs/minerva/resilient/tarballs/rhowell-EventSelection_1000Univ_20251012.tar.gz --scratch
 done
 
 echo "DONE RUNNING AUDIT JOBS"
