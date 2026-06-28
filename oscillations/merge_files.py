21e22d5e4c5bbf58453ddd48958ecad6f7dde85f
Ryan Howell <rhowell@minervagpvm01.fnal.gov>
2026-06-28 17:40:07 -0500

    Adds histogram plot for merged asimovs with critical contour lines if data histogram exists

--- oscillations/merge_files.py
+++ oscillations/merge_files.py
@@ -1,5 +1,22 @@
 import os
+import sys
+import ROOT
+import PlotUtils
+
 import numpy as np
+import math
+from array import array
+
+from tools.StitchedHistogram import *
+from tools.Fitters import *
+ccnueroot = os.environ.get('CCNUEROOT')
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+ROOT.TH1.AddDirectory(False)
+ROOT.SetMemoryPolicy(ROOT.kMemoryStrict)
 
 def MergeAsimovs():
     while True:
@@ -31,6 +48,7 @@ def MergeAsimovs():
             results = np.array(results).flatten()
             print('saving asimov_deltachi2s.npy with {} entries'.format(results.shape[0]))
             np.save("asimov_deltachi2s",results)
+            return(results)
             break
         else:
             print("Not a valid path")
@@ -125,4 +143,51 @@ if __name__ in "__main__":
     print("Do you want to merge asimov delta chi2 files? (y/n)")
     ans = input().lower()
     if ans == 'y':
-        MergeAsimovs()
+        asimovs = MergeAsimovs()
+
+        if False:
+            # Plot a histogram of the asimov delta chi2s with the critical levels marked
+            # First fit to data and use that for the critical chi2s
+            filename = "rootfiles/NuE_stitched_hists.root"
+            
+            file_path = "{}/oscillations/{}".format(ccnueroot,filename)
+
+            sample_histogram = StitchedHistogram("sample")
+            sample_histogram.Load(file_path)
+
+            stat = Statistics(sample_histogram,lam=AnalysisConfig.lambdaValue,exclude=AnalysisConfig.exclude)
+            chi2_null,penalty = stat.Chi2DataMC(marginalize=True)
+            print("null chi2: {:.3f}".format(chi2_null))
+
+            fitter = OscillationFitter(sample_histogram,lam=AnalysisConfig.lambdaValue,exclude=AnalysisConfig.exclude)
+            chi2_fit,res = fitter.DoFit()
+
+            delta_chi2 = chi2_null-chi2_fit
+
+            print("Data fit: delta chi2 = {:.3f} = {:.3f} - {:.3f}".format(delta_chi2,chi2_null,chi2_fit))
+            print("Best fit params:")
+            print("   delta m^2 = {:.3f} eV^2 +- {:.4f}".format(res['m'],0))
+            print("   U_e4^2    = {:.3f}      +- {:.4f}".format(res['ue4'],0))
+            print("   U_mu4^2   = {:.5f}    +- {:.4f}".format(res['umu4'],0))
+            print("   U_tau4^2  = {:.3f}      +- {:.4f}".format(res['utau4'],0))
+
+        delta_chi2 = 1.097
+        #plt.figure(figsize=(8,6))
+        n, bins, patches = plt.hist(asimovs,bins=100,range=(0,100),alpha=0.5,label='Null Hypothesis\nPseudo Experiments',density=True)
+        plt.plot((delta_chi2,delta_chi2),(0,np.max(n)),label='Data $\Delta\chi^2={:.2f}$'.format(delta_chi2))
+        for i in [68,90,95,99]:
+            for j in np.linspace(0,50,1000):
+                c = asimovs[asimovs<j].shape[0]
+                if 100*c/asimovs.shape[0] > i:
+                    plt.plot((j,j),(0,np.max(n)),label="{}% at $\Delta\chi^2={:.2f}$".format(i,j),linestyle='--')
+                    break
+            
+        plt.title("$\Delta\chi^2s$ for Null Hypothesis Pseudo Experiments\n"+"{} Experiments".format(asimovs.shape[0]),fontsize=20)
+        plt.xlabel(r"$\Delta\chi^2=\chi^2(null) - \chi^2_{min}(best\ osc.)$",fontsize=16)
+        plt.ylabel("Frequency",fontsize=16)
+        plt.yscale('log')
+        plt.xlim(0,100)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.legend(fontsize=14)
+        plt.savefig('histogram_plot.png', dpi=300, bbox_inches='tight')
